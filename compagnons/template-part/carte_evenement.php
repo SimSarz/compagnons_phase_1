33635a7ae901362ad586135d0df6ab4c1fb01962
Bryanjobin <Bryanjobin@gmail.com>
2023-11-29 14:50:55 -0500

    EventCard, hyperlien forge en action, marges forge en action, rvgg btn nav, rvgg dropdown deleted, en savoir plus transition

--- compagnons/template-part/carte_evenement.php
+++ compagnons/template-part/carte_evenement.php
@@ -27,8 +27,7 @@ $args = wp_parse_args($args, array(
             <div class="eventCard_infoBox_info">
                 <div class="eventCard_infoBox_info_container">
                     <div class="eventCard_infoBox_info_container_placeNPrice">
-                        <p class="eventCard_infoBox_info_container_lieu"><?php echo $args['lieu']?></p>
-                        <p class="eventCard_infoBox_info_container_placeNPrice_hour"><?php echo $args['heure']?> / <?php echo $args['prix']?></p>
+                        <p class="eventCard_infoBox_info_container_placeNPrice_hour"><?php echo $args['heure']?> / <?php echo $args['prix']?></p>                   
                     </div>
                     <p  class="eventCard_infoBox_info_container_desc"><?php echo $args['desc']?></p>
                 </div>
@@ -39,6 +38,7 @@ $args = wp_parse_args($args, array(
                     <p class="eventCard_tag_dataDate hidden"><?php echo $args['data_date']?></p>
                 </div>
                 <div class="eventCard_infoBox_info_buttonContainer">
+                    <p class="eventCard_infoBox_info_container_title" href=""><?php echo $args['title']?></p>
                     <a class="eventCard_link" href="<?php echo $args['billeterie']?>" target="_blank"><button class="eventCard_infoBox_info_button btn-link bg-white">Billetterie<span class="material-symbols-outlined eventCard_infoBox_info_button_icon">arrow_right_alt</span></button></a>
                 </div>
             </div>
@@ -48,6 +48,8 @@ $args = wp_parse_args($args, array(
         </div>
     </div>
     <div class="eventCard_titleBox">
-        <p class="btn_default"><?php echo $args['title']?></p>
+            <p class="eventCard_infoBox_info_container_title" href=""><?php echo $args['title']?></p>
+            <p class="eventCard_infoBox_info_container_lieu "><?php echo $args['lieu']?></p>
+            <p class="eventCard_infoBox_info_container_placeNPrice_hour"><?php echo $args['heure']?> / <?php echo $args['prix']?></p>
     </div>
 </div>
